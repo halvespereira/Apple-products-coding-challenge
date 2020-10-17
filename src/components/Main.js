15aa551113d4aa365d7f5b8de0724531a41c8a3a
Henrique Pereira <62767337+halvespereira@users.noreply.github.com>
2020-10-16 20:58:44 -0500

    test

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 import LoggedInRoutes from "./routes/LoggedInRoutes";
-// import LoggedOutRoute from "./routes/LoggedOutRoute";
-import Login from "../components/loginPage/Login";
-
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import LoggedOutRoute from "./routes/LoggedOutRoute";
 
 const Main = ({
   email,
@@ -18,24 +15,13 @@ const Main = ({
   // Checking if user is logged in and rendering the adequate component using react-router-dom
   if (!isUser) {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route
-            exact
-            path="/"
-            render={(props) => (
-              <Login
-                {...props}
-                email={email}
-                setEmail={setEmail}
-                setIsUser={setIsUser}
-                password={password}
-                setPassword={setPassword}
-              />
-            )}
-          />
-        </Switch>
-      </BrowserRouter>
+      <LoggedOutRoute
+        email={email}
+        setEmail={setEmail}
+        setIsUser={setIsUser}
+        password={password}
+        setPassword={setPassword}
+      />
     );
   } else {
     return (
